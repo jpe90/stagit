348502ad0b6bc8a9132e2b937f71c49ba0ba233b
Jon Eskin <eskinjp@gmail.com>
2022-10-02 06:53:10 -0400

    add additional readme filenames

--- stagit.c
+++ stagit.c
@@ -71,7 +71,7 @@ static char cloneurl[1024];
 static char *submodules;
 static char *licensefiles[] = { "HEAD:LICENSE", "HEAD:LICENSE.md", "HEAD:COPYING" };
 static char *license;
-static char *readmefiles[] = { "HEAD:README", "HEAD:README.md" };
+static char *readmefiles[] = { "HEAD:README", "HEAD:README.md", "HEAD:readme", "HEAD:readme.md" };
 static char *readme;
 static long long nlogcommits = -1; /* -1 indicates not used */
 
